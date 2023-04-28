e2e1896a115313d7c943588f716d6439b68bab98
poornachander007 <poornachander0333@gmail.com>
2023-04-28 08:23:07 +0000

    solution

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import {Component} from 'react'
+import MemeGenerator from './components/MemeGenerator'
+
+import './App.css'
+
+const App = () => <MemeGenerator />
+
+export default App
+
+// class App extends Component {
+//   state = {}
+
+//   render() {
+//     return <MemeGenerator/>
+//   }
+// }
